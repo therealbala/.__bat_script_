aac734bf7a619f8a28eab1c35e187b8fa8afcc1e
Mr. B <75065725+therealbala@users.noreply.github.com>
2023-04-03 11:15:49 +0530

    Add files via upload

---
+++ 🐍 run-python (drag here).bat
@@ -0,0 +1,15 @@
+title Python 
+:loop 
+cls
+
+@echo off
+:: echo ##### This is Python Program #####
+:: echo ##### Simple Drag&Drop Py files only #####
+
+py %1 
+
+:: echo ##### This is Python Program ##### 
+pause>nul  
+
+goto loop
+
